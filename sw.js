313ea2d07c50654ad41b8978f23b5a1d7b1c91d2
Ardi <renaldimedia@gmail.com>
2019-10-19 03:05:07 +0700

    recompile

--- sw.js
+++ sw.js
@@ -28,5 +28,5 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('/sample-marketplace/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/sample-marketplace/nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
 workbox.routing.registerRoute(new RegExp('/sample-marketplace/'), new workbox.strategies.NetworkFirst ({}), 'GET')
